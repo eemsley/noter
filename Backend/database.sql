b3823dc2658806dded1a3da5254465ccd44e8504
eemsley <eemsley@purdue.edu>
2023-11-07 13:12:34 -0500

    added sharing

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,7 +1,20 @@
 CREATE DATABASE noter;
+/* create a table users which has a username as the primary key and password*/
 
+CREATE TABLE users(
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255)
+);
 
 CREATE TABLE notes(
     noteId SERIAL PRIMARY KEY,
-    text VARCHAR(255)
+    text VARCHAR(255),
+    createdat TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR(255) REFERENCES users(username)
+);
+CREATE TABLE sharedNotes(
+    sharedId SERIAL PRIMARY KEY,
+    authorName VARCHAR(255) REFERENCES users(username),
+    sharedWithName VARCHAR(255) REFERENCES users(username),
+    noteId INTEGER REFERENCES notes(noteId)
 );
--- Backend/index.js
+++ Backend/index.js
@@ -9,13 +9,127 @@ app.use(express.json());
 
 //ROUTES
 
+//get all shared notes for a user
+app.get("/users/:username/sharedNotes", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const note = await pool.query(
+      "SELECT * FROM sharedNotes s join notes n ON n.noteid = s.noteid WHERE sharedWithName = $1;",
+      [username]
+    );
+    res.json(note.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all users a note is shared with
+app.get("/notes/:id/sharedWith", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const note = await pool.query(
+      "SELECT sharedwithname FROM sharedNotes WHERE noteId = $1;",
+      [id]
+    );
+    res.json(note.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//share note
+app.post("/notes/:id/share", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { authorName, sharedWithName } = req.body;
+    //check if note is already shared with user
+    if (authorName === sharedWithName) {
+      res.json("You can't share a note with yourself!");
+      return;
+    }
+
+    const alreadyShared = await pool.query(
+      "SELECT * FROM sharedNotes WHERE noteId = $1 AND sharedWithName = $2",
+      [id, sharedWithName]
+    );
+    if (alreadyShared.rows[0]) {
+      //unshare note
+      const unshareNote = await pool.query(
+        "DELETE FROM sharedNotes WHERE noteId = $1 AND sharedWithName = $2",
+        [id, sharedWithName]
+      );
+      res.json("Note " + id + " Unshared!");
+      return;
+    } else {
+      const shareNote = await pool.query(
+        "INSERT INTO sharedNotes (authorName, sharedWithName, noteId) VALUES($1, $2, $3) RETURNING *",
+        [authorName, sharedWithName, id]
+      );
+      res.json(shareNote.rows[0]);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all users
+app.get("/users", async (req, res) => {
+  try {
+    const allUsers = await pool.query("SELECT * FROM users;");
+    res.json(allUsers.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//create a user
+app.post("/users", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO users (username, password) VALUES($1, $2) RETURNING *",
+      [username, password]
+    );
+    res.json({ user: newUser.rows[0], taken: false });
+  } catch (err) {
+    console.log(JSON.stringify(err));
+    res.json({ user: null, taken: true });
+  }
+});
+//login a user
+app.post("/users/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userExists = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    const user = await pool.query(
+      "SELECT * FROM users WHERE username = $1 AND password = $2",
+      [username, password]
+    );
+    if (user.rows[0]) {
+      res.json({ user: user.rows[0], loggedIn: true, userExists: true });
+    } else {
+      if (userExists.rows[0]) {
+        res.json({ user: null, loggedIn: false, userExists: true });
+      } else {
+        res.json({ user: null, loggedIn: false, userExists: false });
+      }
+    }
+  } catch (err) {
+    console.log(JSON.stringify(err));
+    res.json({ user: null, loggedIn: false });
+  }
+});
+
 //create a note
 app.post("/notes", async (req, res) => {
   try {
-    const { text } = req.body;
+    const { text, username } = req.body;
     const newNote = await pool.query(
-      "INSERT INTO notes (text) VALUES($1) RETURNING *",
-      [text]
+      "INSERT INTO notes (text, username) VALUES($1, $2) RETURNING *",
+      [text, username]
     );
     res.json(newNote.rows[0]);
   } catch (err) {
@@ -45,6 +159,18 @@ app.get("/notes/:id", async (req, res) => {
     console.error(err.message);
   }
 });
+//get a username's notes
+app.get("/users/:username/notes", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const note = await pool.query("SELECT * FROM notes WHERE username = $1;", [
+      username,
+    ]);
+    res.json(note.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //update a note
 app.put("/notes/:id", async (req, res) => {
@@ -65,7 +191,9 @@ app.put("/notes/:id", async (req, res) => {
 app.delete("/notes/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteTodo = pool.query("DELETE FROM notes WHERE noteId=$1", [id]);
+    const deleteTodo = pool.query("DELETE FROM notes WHERE noteId=$1 CASCADE", [
+      id,
+    ]);
     res.json("Note " + id + " Deleted!");
   } catch (err) {
     console.error(err.message);
